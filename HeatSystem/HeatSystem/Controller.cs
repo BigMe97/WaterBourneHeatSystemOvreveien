1da231f13d3dc710c94fe375d52c133c9f79873a
BigMe97 <magnuseide@msn.com>
2023-04-25 18:01:34 +0200

    Elaboration Phase

---
+++ HeatSystem/HeatSystem/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeatSystem
+{
+    internal class Controller
+    {
+
+        public Controller(DataAccess data) { }
+
+
+
+
+    }
+}
---
+++ HeatSystem/HeatSystem/UserInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeatSystem
+{
+    internal class UserInterface
+    {
+        public bool Run(bool run)
+        {
+
+            return run;
+        }
+        public UserInterface(Facade controller) { }
+    }
+}
